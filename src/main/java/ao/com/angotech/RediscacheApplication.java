a11a83d05a51d6ee4039bc93f41b985598d54122
fernando-angolar <fernandoangolar581@gmail.com>
2026-04-03 15:37:52 +0100

    feat/task1-inicializacao-spring-redis

---
+++ src/main/java/ao/com/angotech/RediscacheApplication.java
@@ -0,0 +1,13 @@
+package ao.com.angotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RediscacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RediscacheApplication.class, args);
+	}
+
+}
